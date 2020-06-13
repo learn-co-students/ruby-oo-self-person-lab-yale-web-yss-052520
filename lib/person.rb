74d354ca39bb31c4539ee7ae5a7fb61d57fa73d8
CelestialEmperor <mapeiyao@gmail.com>
2020-06-13 06:00:48 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,68 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(number)
+        @happiness = number
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+    end
+
+    def hygiene=(number)
+        @hygiene = number
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        people = [self, friend]
+        if topic == "politics"
+            people.each{ |person| person.happiness -= 2}
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            people.each{ |person| person.happiness += 1}
+            return "blah blah sun blah rain"
+        else topic == "other"
+            return "blah blah blah blah blah"
+        end
+    end
+
+end
